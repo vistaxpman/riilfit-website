103a21c88fc050b758469f7e9c6ec115a8ba7c86
Princewill Iroka <princewilliroka@outlook.com>
2021-04-19 16:15:46 +0100

    Added 2nd section to Nhood

--- pages/nhood.js
+++ pages/nhood.js
@@ -12,7 +12,7 @@ export default function Nhood() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Wrapper className="sleek-scrollbar">
-        <header className="h-20 bg-black fixed top-0 w-full flex items-center">
+        <header className="h-20 bg-black fixed top-0 w-full flex items-center z-10">
           <img
             className="mx-auto w-16 h-16 object-contain"
             src="/assets/homepage/nhood-logo.svg"
@@ -29,7 +29,7 @@ export default function Nhood() {
             </ul>
           </nav>
         </header>
-        <section className="mt-20">
+        <section className="pt-20">
           <div className="relative w-full h-128">
             <img
               className="mx-auto w-full h-full object-cover"
@@ -49,12 +49,44 @@ export default function Nhood() {
                 possible rig equipment to your specifications.
               </span>
               <div className="flex items-center">
-                <span className="flex justify-center items-center rounded-full w-40 py-2 text-white bg-custom-104 mr-8">
+                <button className="flex justify-center items-center rounded-full w-40 py-2 text-white bg-custom-106 mr-8">
                   Get Started
-                </span>
-                <span className="flex justify-center items-center rounded-full w-40 py-2 text-white border-1 border-custom-104">
+                </button>
+                <button className="flex justify-center items-center rounded-full w-40 py-2 text-white border-1 border-custom-106">
                   Make a Booking
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="flex">
+            <div className="w-1/2 bg-custom-106 h-80 flex justify-center items-center">
+              <div className="flex flex-col w-1/2.5">
+                <span className="text-3xl font-bold text-white uppercase leading-8 mb-4">
+                  Membership start from NGN8,000/m
                 </span>
+                <span className="text-sm text-white mb-4">
+                  We make fitness fun, effective and simple. Our coaches lead
+                  the fitness industry when it comes to helping people get into
+                  the best shape of their lives and reaching
+                </span>
+                <button className="text-white bg-black w-40 rounded-full py-3">
+                  Book Now
+                </button>
+              </div>
+            </div>
+            <div className="w-1/2 bg-black h-80 flex justify-center items-center">
+              <div className="flex flex-col w-1/2.5">
+                <span className="text-3xl font-bold text-white uppercase leading-8 mb-4">
+                  Join Today &amp; Get some of Free package
+                </span>
+                <span className="text-sm text-white mb-4">
+                  We make fitness fun, effective and simple. Our coaches lead
+                  the fitness industry when it comes to helping people get into
+                  the best shape of their lives and reaching
+                </span>
+                <button className="text-white bg-custom-106 w-40 rounded-full py-3">
+                  Book Now
+                </button>
               </div>
             </div>
           </div>
